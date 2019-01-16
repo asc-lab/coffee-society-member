e001a9c9d394b67cabae58ee11f95b3acaeb6076
Robert Kuśmierek <robert.kusmierek@altkom.pl>
2019-01-16 20:44:05 +0100

    add user id to user data

--- members-api/src/main/kotlin/pl/altkom/coffee/members/api/dto/UserDto.kt
+++ members-api/src/main/kotlin/pl/altkom/coffee/members/api/dto/UserDto.kt
@@ -3,5 +3,6 @@ package pl.altkom.coffee.members.api.dto
 import pl.altkom.coffee.members.api.enums.UserRole
 
 data class UserDto(
+        val id: String,
         val username: String,
         val roles: List<UserRole>)
